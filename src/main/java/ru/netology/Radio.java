caeadca107b85c7dd55edb76266d11e06d1bfad5
MariaDzha <m_zudina@inbox.ru>
2022-01-17 00:01:47 +0300

    New requirements changes

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -3,13 +3,26 @@ package ru.netology;
 public class Radio {
     private int currentVolume = 0;
     private int currentStation = 0;
+    private int stationCount;
+    private int maxStation;
+
+
+    public Radio(int stationCount) {
+        this.stationCount = stationCount;
+        this.maxStation = stationCount - 1;
+    }
+
+    public Radio() {
+        stationCount = 10;
+        maxStation = 9;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
     public void increaseVolume() {
-        if (currentVolume == 10) {
+        if (currentVolume == 100) {
             return;
         }
         currentVolume = currentVolume + 1;
@@ -30,7 +43,7 @@ public class Radio {
     }
 
     public void nextStation() {
-        if (currentStation < 9) {
+        if (currentStation < maxStation) {
             currentStation = currentStation + 1;
         } else {
             currentStation = 0;
@@ -41,12 +54,12 @@ public class Radio {
         if (currentStation > 0) {
             currentStation = currentStation - 1;
         } else {
-            currentStation = 9;
+            currentStation = maxStation;
         }
     }
 
     public void setCurrentStation(int currentStation) {
-        if (0 <= currentStation && currentStation <= 9) {
+        if (0 <= currentStation && currentStation <= maxStation) {
             this.currentStation = currentStation;
         }
     }
